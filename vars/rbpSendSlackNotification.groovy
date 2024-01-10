539397fcd63eb64879663b3e2ecd01064e2a21f1
Vincenzo Tramo <vv.tramo@gmail.com>
2024-01-10 23:21:54 +0100

    feat: add rbpSendSlackNotification custom step

---
+++ vars/rbpSendSlackNotification.groovy
@@ -0,0 +1,23 @@
+def call(String serviceName) {
+    def (slackColor, slackMessage) =
+        SlackUtilities.getSlackColorAndMessageNotificationByBuildResult(BuildResult.valueOf(currentBuild.result))
+    def ciChannel = SlackUtilities.CI_CHANNEL
+
+    slackSend(
+        channel: "${ciChannel}",
+        color: "${slackColor}",
+        message: """
+            :white_check_mark: [${serviceName}] ${slackMessage}
+            *Branch:* ${GIT_BRANCH}
+            *Commit ID:* ${GIT_COMMIT}
+            *Short commit ID:* ${GIT_SHORT_COMMIT}
+            *Commit message:* ${GIT_COMMIT_MSG}
+            *Previous successful commit ID:* ${GIT_PREVIOUS_SUCCESSFUL_SHORT_COMMIT} 
+            *Committer name:* ${GIT_COMMITTER_NAME}
+            *Committer email:* ${GIT_COMMITTER_EMAIL}
+            *Build label:* ${BUILD_TAG}
+            *Build ID:* ${BUILD_ID}
+            *Build URL:* ${BUILD_URL}
+        """
+    )
+}
