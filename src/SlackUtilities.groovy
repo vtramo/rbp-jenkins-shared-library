86ddaf7476a21de5f0c14b169c6d9c7143202a69
Vincenzo Tramo <vv.tramo@gmail.com>
2024-01-12 11:08:43 +0100

    feat: add support for aborted builds in Slack notifications

--- src/SlackUtilities.groovy
+++ src/SlackUtilities.groovy
@@ -8,10 +8,12 @@ class SlackUtilities {
     static String SUCCESS_MESSAGE = "Build was successful!"
     static String FAILURE_MESSAGE = "Build failed!"
     static String UNSTABLE_MESSAGE = "Unstable build!"
+    static String ABORTED_MESSAGE = "Build was aborted!"
 
     static String SUCCESS_EMOTICON = ":white_check_mark:"
     static String FAILURE_EMOTICON = ":x:"
     static String UNSTABLE_EMOTICON = ":warning:"
+    static String ABORTED_EMOTICON = ":no_entry:"
 
     static List<String> buildSlackMessageByBuildResult(BuildStatus buildResult) {
         switch(buildResult) {
@@ -21,6 +23,8 @@ class SlackUtilities {
                 return [FAILURE_EMOTICON, RED, FAILURE_MESSAGE]
             case BuildStatus.UNSTABLE:
                 return [UNSTABLE_EMOTICON, YELLOW, UNSTABLE_MESSAGE]
+            case BuildStatus.ABORTED:
+                return [ABORTED_EMOTICON, YELLOW, ABORTED_MESSAGE]
             default:
                 throw new RuntimeException("Unexpected build result: " + buildResult)
         }
